6e9655ce9f473c2599775d0645a61ea7d58be034
talhaozbay <oyun_kolu_44@hotmail.com>
2021-08-04 13:28:44 +0300

    Controller and service update = getmappings

--- src/main/java/com/project/product/controller/ProductController.java
+++ src/main/java/com/project/product/controller/ProductController.java
@@ -1,25 +1,29 @@
-package com.project.product.controller;
+package com.project.product.Controller;
 
 import java.util.List;
 
-import com.project.product.Repository.ProductRepository;
+import com.project.product.Service.ProductService;
 import com.project.product.entity.Product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class ProductController {
+public class ProductController {
 
-    private final ProductRepository repository;
+    @Autowired
+    ProductService productService;
 
-    ProductController(ProductRepository repository) {
-        this.repository = repository;
+    @GetMapping("/product")
+    private List<Product> getAllProducts() {
+        return productService.getAllProducts();
     }
 
-    @GetMapping("/products")
-    List<Product> all() {
-        return repository.findAll();
-    }
+    @PostMapping("/products")
+    private int saveproduct(@RequestBody Product products) {
 
+    }
 }
