c6cc88ad92b09f5e499b2ab59c91fe00d9480bf6
talhaozbay <oyun_kolu_44@hotmail.com>
2021-08-05 14:53:16 +0300

    Projecet completed but cannot start

--- src/main/java/com/project/product/Service/ProductService.java
+++ src/main/java/com/project/product/Service/ProductService.java
@@ -1,5 +1,43 @@
 package com.project.product.Service;
 
+import java.util.*;
+
+import com.project.product.Repository.ProductRepository;
+import com.project.product.entity.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+    //get
+    public List<Product> getAllProducts() {
+        List<Product> products = new ArrayList<Product>();
+        productRepository.findAll().forEach(product1 -> products.add(product1));
+        return products;
+    }
+    //get id
+    public Product getProductById(Long id){
+        return productRepository.findById(id).get();
+    }
+    //post
+    public void saveOrUpdate(Product product) {
+    productRepository.save(product);
+    }
+    //delete all
+    public void deleteAll(Product product){
+        productRepository.deleteAll();
+    }
+    //delete id
+    public void delete(Long id){
+        productRepository.deleteById(id);
+    }
+    //update
+    // public void update(Product product , Long productId){
+    //     productRepository.save(product);
+    // }
 
 }
--- src/main/java/com/project/product/controller/ProductController.java
+++ src/main/java/com/project/product/controller/ProductController.java
@@ -6,9 +6,13 @@ import com.project.product.Service.ProductService;
 import com.project.product.entity.Product;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,14 +20,38 @@ public class ProductController {
 
     @Autowired
     ProductService productService;
-
+    //get
     @GetMapping("/product")
     private List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
-
-    @PostMapping("/products")
-    private int saveproduct(@RequestBody Product products) {
-
+    //get id
+    @GetMapping("/product/{productId}")
+    private Product getProduct(@PathVariable("productId")Long productId){
+        return productService.getProductById(productId);
     }
+    //post
+    @PostMapping("/product")
+    private Long saveproduct(@RequestBody Product product) {
+    productService.saveOrUpdate(product);
+    return product.getProductId();
+    }
+    //delete all
+    @DeleteMapping("/product")
+    private void deleteAllProduct(@RequestBody Product product){
+        productService.deleteAll(product);
+    }
+    //delete id
+    @DeleteMapping("/product/{productId}")
+    private void deleteProduct(@PathVariable("productId") Long productId){
+        productService.delete(productId);
+    }
+    //update
+    // @PutMapping("/product")
+    // private Product update(@RequestBody Product product){
+    //     productService.saveOrUpdate(product);
+    //     return product;
+    // }
+
+
 }
